5210676694f77b9fa9f7c44ff7a21272f5ed4fc2
Yago Sodre <yagobk@gmail.com>
2024-03-11 22:29:50 -0300

    refactor: db connection

--- app/conexao-db.php
+++ app/conexao-db.php
@@ -1,10 +1,23 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once(__DIR__ . "/../vendor/autoload.php");
+
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
-$pdo = new PDO("mysql:host=" . $_ENV['DATABASE_HOST'] . ";dbname=" . $_ENV['DATABASE_DB'],
-     $_ENV['DATABASE_USER'],
-     $_ENV['DATABASE_PASSWORD']);
+$capsule = new Capsule;
+$capsule->addConnection([
+   "driver" => "mysql",
+   "host" => $_ENV["DATABASE_HOST"],
+   "database" => $_ENV["DATABASE_DB"],
+   "username" => $_ENV["DATABASE_USER"],
+   "password" => $_ENV["DATABASE_PASSWORD"]
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
