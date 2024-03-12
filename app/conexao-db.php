c862c425dd3f50e61fc8d5a959f8c9d0f388d27b
Yago Sodre <yagobk@gmail.com>
2024-03-12 08:37:20 -0300

    fix: conexao-db.php

--- app/conexao-db.php
+++ app/conexao-db.php
@@ -4,18 +4,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once(__DIR__ . "/../vendor/autoload.php");
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->safeLoad();
 
 $capsule = new Capsule;
 $capsule->addConnection([
    "driver" => "mysql",
-   "host" => $_ENV["DATABASE_HOST"],
-   "database" => $_ENV["DATABASE_DB"],
-   "username" => $_ENV["DATABASE_USER"],
-   "password" => $_ENV["DATABASE_PASSWORD"]
+   "host" =>  getenv("DATABASE_HOST"),
+   "database" =>  getenv("DATABASE_DB"),
+   "username" =>  getenv("DATABASE_USER"),
+   "password" =>  getenv("DATABASE_PASSWORD")
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
+
+var_dump($capsule);
+exit();
