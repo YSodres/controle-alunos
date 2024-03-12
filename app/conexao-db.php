3fa2ce720f74543a4c0a7ca97e6beff537785b10
Yago Sodre <yagobk@gmail.com>
2024-03-12 08:21:35 -0300

    refactor: conexao-db

--- app/conexao-db.php
+++ app/conexao-db.php
@@ -4,8 +4,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once(__DIR__ . "/../vendor/autoload.php");
-
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
